6417cfb1c3382335daef10b2d0fa1761fc5f1620
arorayash <arorayash.thisisus@gmail.com>
2019-01-09 11:07:34 +0530

    3d grid

--- webgl.js
+++ webgl.js
@@ -1,76 +1,109 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
+const palettes = require("nice-color-palettes");
+import random from "canvas-sketch-util/random";
+import eases from "eases";
 
 // Ensure ThreeJS is in global scope for the 'examples/'
-global.THREE = require('three');
+global.THREE = require("three");
 
 // Include any additional ThreeJS examples below
-require('three/examples/js/controls/OrbitControls');
+require("three/examples/js/controls/OrbitControls");
 
 const settings = {
+  dimensions: [512, 512],
+  duration: 4,
+  fps: 24,
   // Make the loop animated
   animate: true,
   // Get a WebGL canvas rather than 2D
-  context: 'webgl',
+  context: "webgl",
   // Turn on MSAA
   attributes: { antialias: true }
 };
 
 const sketch = ({ context }) => {
+  const palette = random.pick(palettes);
   // Create a renderer
   const renderer = new THREE.WebGLRenderer({
     context
   });
 
   // WebGL background color
-  renderer.setClearColor('#000', 1);
+  renderer.setClearColor("hsl(0, 0%, 95%)", 1);
 
   // Setup a camera
-  const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 100);
-  camera.position.set(2, 2, -4);
-  camera.lookAt(new THREE.Vector3());
-
-  // Setup camera controller
-  const controls = new THREE.OrbitControls(camera);
+  const camera = new THREE.OrthographicCamera();
 
   // Setup your scene
   const scene = new THREE.Scene();
 
-  const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshPhysicalMaterial({
-      color: 'white',
-      roughness: 0.75,
-      flatShading: true
-    })
-  );
-  scene.add(mesh);
+  const box = new THREE.BoxGeometry(1, 1, 1);
+  for (let i = 0; i < 100; i++) {
+    const mesh = new THREE.Mesh(
+      box,
+      new THREE.MeshStandardMaterial({
+        color: random.pick(palette),
+        flatShading: true
+      })
+    );
+    mesh.position.set(
+      random.range(-1, 1),
+      random.range(-1, 1),
+      random.range(-1, 1)
+    );
+    mesh.scale.set(
+      random.range(-1, 1),
+      random.range(-1, 1),
+      random.range(-1, 1)
+    );
+    mesh.scale.multiplyScalar(0.25);
+    scene.add(mesh);
+  }
 
-  // Specify an ambient/unlit colour
-  scene.add(new THREE.AmbientLight('#59314f'));
+  const ambientLight = new THREE.AmbientLight("white"); // to fill where light doesn't go
+  scene.add(ambientLight);
 
-  // Add some light
-  const light = new THREE.PointLight('#45caf7', 1, 15.5);
-  light.position.set(2, 2, -4).multiplyScalar(1.5);
+  const light = new THREE.DirectionalLight("white", 1);
+  light.position.set(0, 0, 4);
   scene.add(light);
 
   // draw each frame
   return {
     // Handle resize events here
-    resize ({ pixelRatio, viewportWidth, viewportHeight }) {
+    resize({ pixelRatio, viewportWidth, viewportHeight }) {
       renderer.setPixelRatio(pixelRatio);
       renderer.setSize(viewportWidth, viewportHeight);
-      camera.aspect = viewportWidth / viewportHeight;
+
+      const aspect = viewportWidth / viewportHeight;
+
+      // Ortho zoom
+      const zoom = 2.0;
+
+      // Bounds
+      camera.left = -zoom * aspect;
+      camera.right = zoom * aspect;
+      camera.top = zoom;
+      camera.bottom = -zoom;
+
+      // Near/Far
+      camera.near = -100;
+      camera.far = 100;
+
+      // Set position & look at world center
+      camera.position.set(zoom, zoom, zoom);
+      camera.lookAt(new THREE.Vector3());
+
+      // Update the camera
       camera.updateProjectionMatrix();
     },
     // Update & render your scene here
-    render ({ time }) {
-      mesh.rotation.y = time * (10 * Math.PI / 180);
-      controls.update();
+    render({ playhead }) {
+      const t = Math.sin(playhead * Math.PI);
+      scene.rotation.z = eases.expoInOut(t);
       renderer.render(scene, camera);
     },
     // Dispose of events & renderer for cleaner hot-reloading
-    unload () {
-      controls.dispose();
+    unload() {
       renderer.dispose();
     }
   };
